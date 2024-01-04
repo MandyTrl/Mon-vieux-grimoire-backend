7c87aea25642ab9a3af420263bae92681e0c7a43
MThorel <mthorel@corp.free.fr>
2024-01-04 12:45:09 +0100

    fix syntaxe

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken') //import du package jsonwebtoken
 
-module.exports = (req, res, next) => {
+module.exports = (req, res) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1] //récupère le token dans le headers de la requête et coupe après le mot "Bearer" la bonne valeur
 		const decodedToken = jwt.verify(token, 'TOKEN_KEY') //on décode le token avec la méthode ".verify()" du package jsonwebtoken en lui passant en 2nd argument la clé secret
