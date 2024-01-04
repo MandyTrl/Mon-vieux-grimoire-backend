3ea3a692f8587c6e9f848daca6938a04759078f9
MThorel <mthorel@corp.free.fr>
2024-01-04 12:24:49 +0100

    create authentification middleware + add it to specifics routes

---
+++ middlewares/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken') //import du package jsonwebtoken
+
+module.exports = (req, res, next) => {
+	try {
+		const token = req.headers.authorization.split(' ')[1] //récupère le token dans le headers de la requête et coupe après le mot "Bearer" la bonne valeur
+		const decodedToken = jwt.verify(token, 'TOKEN_KEY') //on décode le token avec la méthode ".verify()" du package jsonwebtoken en lui passant en 2nd argument la clé secret
+		const userId = decodedToken.userId
+		req.auth = {
+			userID: userID,
+		}
+	} catch (error) {
+		res.status(401).json({ error }) //erreur d'accès
+	}
+}
--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,10 @@
 const express = require('express') //import d'express
 const router = express.Router() //création du router avec la méthode ".router()" fournie par Express
 const bookControllers = require('../controllers/book.js') //export du controller "book"
+const authMiddleware = require('../middleware/auth') //export du middleware d'auhtentification
 
-//ajoute un livre à la BDD
-router.post('/', bookControllers.addBook)
+//ajoute un livre à la BDD - besoin d'être authentifié
+router.post('/', authMiddleware, bookControllers.addBook)
 
 //récupére tous les livres de la BDD
 router.get('/', bookControllers.getBooks)
@@ -11,14 +12,14 @@ router.get('/', bookControllers.getBooks)
 //récupére un livre
 router.get('/:id', bookControllers.searchBook)
 
-//modifie un livre à la BDD
-router.put('/:id', bookControllers.updateBook)
+//modifie un livre à la BDD - besoin d'être authentifié
+router.put('/:id', authMiddleware, bookControllers.updateBook)
 
-//supprime un livre à la BDD
-router.delete('/:id', bookControllers.deleteBook)
+//supprime un livre à la BDD - besoin d'être authentifié
+router.delete('/:id', authMiddleware, bookControllers.deleteBook)
 
-//ajoute une notation à un livre à la BDD
-router.put('/:id/rating', bookControllers.addNotation)
+//ajoute une notation à un livre à la BDD - besoin d'être authentifié
+router.put('/:id/rating', authMiddleware, bookControllers.addNotation)
 
 //renvoie le top 3 des livres les mieux évalués
 router.get('/bestrating', bookControllers.top3)
