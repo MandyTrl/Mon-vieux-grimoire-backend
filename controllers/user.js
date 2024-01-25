558407fbcb2ae81348f038aecc71abcc61d98d54
MThorel <mthorel@corp.free.fr>
2024-01-25 12:13:49 +0100

    add password-validator package + handle password's errors

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,39 @@
 const bcrypt = require('bcrypt') //import du package bcrypt
 const jwt = require('jsonwebtoken') //import du package jsonwebtoken
 const emailValidator = require('email-validator') //import du package email-validator
+const passwordValidator = require('password-validator') //import du package password-validator
 const User = require('../models/user') //import du model "user"
+
+//création d'un schema de mdp
+const passwordSchema = new passwordValidator()
+passwordSchema
+	.is()
+	.min(8) //min. 8 caractères
+	.is()
+	.max(100) //max. 100 caractères
+	.has()
+	.uppercase() //doit contenir 1 majuscule
+	.has()
+	.lowercase() //doit contenir 1 minuscule
+	.has()
+	.digits() //doit contenir 1 chiffre
+	.has()
+	.not()
+	.spaces() //ne doit pas contenir d'espaces
 
 //crée un nouvel utilisateur
 exports.signUp = (req, res) => {
 	//on vérifie que le format de l'email est bien valide
 	if (!emailValidator.validate(req.body.email)) {
 		return res.status(400).json({ error: 'Em@il non valide' })
+	}
+
+	//vérifie si le mdp respecte le schéma défini pour + de sécurité
+	if (!passwordSchema.validate(req.body.password)) {
+		return res.status(400).json({
+			error:
+				"Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et ne pas contenir d'espaces.",
+		})
 	}
 
 	bcrypt
